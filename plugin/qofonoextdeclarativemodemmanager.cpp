0c22b7bfed524803eeb777318d1fafe60582fa2e
Slava Monich <slava.monich@jolla.com>
2015-11-13 10:28:27 +0200

    Housekeeping

--- plugin/qofonoextdeclarativemodemmanager.cpp
+++ plugin/qofonoextdeclarativemodemmanager.cpp
@@ -1,12 +1,22 @@
-/*
-    Copyright (C) 2015 Jolla Ltd.
-    Contact: Aaron McCarthy <aaron.mccarthy@jollamobile.com>
-*/
+/****************************************************************************
+**
+** Copyright (C) 2015 Jolla Ltd.
+** Contact: Aaron McCarthy <aaron.mccarthy@jollamobile.com>
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
 
 #include "qofonoextdeclarativemodemmanager.h"
 
-QOfonoExtDeclarativeModemManager::QOfonoExtDeclarativeModemManager(QObject *parent)
-:   QOfonoExtModemManager(parent)
+QOfonoExtDeclarativeModemManager::QOfonoExtDeclarativeModemManager(QObject* aParent) :
+    QOfonoExtModemManager(aParent)
 {
     connect(this, SIGNAL(presentSimsChanged(QOfonoExtBoolList)),
             this, SIGNAL(presentSimsChanged()));
@@ -19,7 +29,8 @@ QOfonoExtDeclarativeModemManager::~QOfonoExtDeclarativeModemManager()
 QVariantList QOfonoExtDeclarativeModemManager::presentSims() const
 {
     QVariantList list;
-    foreach (bool present, QOfonoExtModemManager::presentSims())
+    foreach (bool present, QOfonoExtModemManager::presentSims()) {
         list.append(present);
+    }
     return list;
 }
--- plugin/qofonoextdeclarativemodemmanager.h
+++ plugin/qofonoextdeclarativemodemmanager.h
@@ -1,7 +1,17 @@
-/*
-    Copyright (C) 2015 Jolla Ltd.
-    Contact: Aaron McCarthy <aaron.mccarthy@jollamobile.com>
-*/
+/****************************************************************************
+**
+** Copyright (C) 2015 Jolla Ltd.
+** Contact: Aaron McCarthy <aaron.mccarthy@jollamobile.com>
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
 
 #ifndef QOFONOEXTDECLARATIVEMODEMMANAGER_H
 #define QOFONOEXTDECLARATIVEMODEMMANAGER_H
@@ -11,11 +21,10 @@
 class QOfonoExtDeclarativeModemManager : public QOfonoExtModemManager
 {
     Q_OBJECT
-
     Q_PROPERTY(QVariantList presentSims READ presentSims NOTIFY presentSimsChanged)
 
 public:
-    QOfonoExtDeclarativeModemManager(QObject *parent = 0);
+    QOfonoExtDeclarativeModemManager(QObject* aParent = NULL);
     ~QOfonoExtDeclarativeModemManager();
 
     QVariantList presentSims() const;
